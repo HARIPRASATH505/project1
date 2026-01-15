fd8b09b7f7594ba9953eb721a981acf259df3634
Hari Prasath <hariprasanthkcse2326@ksrce.ac.in>
2026-01-15 19:12:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function playGame(userChoice) {
+
+    const choices = ["rock", "paper", "scissors"];
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+
+    document.getElementById("userChoice").innerText =
+        "Your Choice: " + userChoice;
+
+    document.getElementById("computerChoice").innerText =
+        "Computer Choice: " + computerChoice;
+
+    let result = "";
+
+    if (userChoice === computerChoice) {
+        result = "It's a Draw 🤝";
+    } 
+    else if (
+        (userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "paper" && computerChoice === "rock") ||
+        (userChoice === "scissors" && computerChoice === "paper")
+    ) {
+        result = "You Win 🎉";
+    } 
+    else {
+        result = "Computer Wins 😢";
+    }
+
+    document.getElementById("winner").innerText = result;
+}
